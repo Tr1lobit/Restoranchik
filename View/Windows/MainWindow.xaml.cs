44d08af4cd9296a80410dc224e6e26c85a07a242
Антон Афоничев <pushatrol@gmail.com>
2023-02-06 14:42:39 +0300

    Добавление обработчика событий для кнопки выхода с главного экрана

--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace RestoApp_Afonichev
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
